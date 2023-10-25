1e5d0d8e79e0b6e214750787bd92491303127192
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-10-25 18:13:24 +0300

    Оптимизирона работа добавлен вывод сообщений о действиях

--- insta_bot_2.py
+++ insta_bot_2.py
@@ -5,18 +5,29 @@ import random
 from database_controller import get_target, delet_target
 user = input('Ведите имя  пользоватея  ').strip()
 password = input('Ведите ваш пароль  ').strip()
+print('Пытаюсь войти  в систему')
 target = []
 get_target(target)
+print('Знаю кого нужно отписать')
 bot = Bot()
 bot.login(username = user, password = password )
+print('Бот вошел  в аккаун')
+time.sleep(random.randrange(5-10))
 counter = len(target)
+time.sleep(random.randrange(5-10))
 for follower in target:
-    bot.get_user_followers(follower)
+    time.sleep(random.randrange(5-10))
     bot.unfollow(follower)
     time.sleep(random.randrange(5-10))
     delet_target(follower)
+    time.sleep(random.randrange(5-10))
     counter -=1
+    time.sleep(random.randrange(5-10))
     print(f'Осталось {counter} oтписок')
-    if counter == 0:
-        bot.logout()
+if counter == 0:
+    print('Аккаунты отписаны')
+if counter == len(target):
+    print('Вы не были подписаны на этих людей')
+    
+bot.logout()
 
